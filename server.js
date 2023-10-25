a781008e46ba7c4c4ebd410f6dec11e69b35aa61
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-10-25 13:16:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
+
 const {join}  = require('path');
+const fs = require('fs');
 
 
 app.use(helmet({crossOriginResourcePolicy: false}));
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'","*.fontawesome.com"],
+        scriptSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"],
+        styleSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"]
+    }
+}));
 
 app.use(cors({
     allowedHeaders: '*',
@@ -15,9 +24,13 @@ app.use(cors({
 }
 ));
 
+
+app.use(express.static(join(__dirname, './public')));
 app.use('/api/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
 app.use('/api/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
-
+app.get('*', (req,res) =>{
+    res.sendFile(join(__dirname,'./public/index.html'));
+});
 
 app.listen(3000,()=>console.log('API gateway listen on port 3000'));
 
