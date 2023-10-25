636921741c925dac10159ff5b2bd14a302330087
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-10-25 10:32:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const fs = require('fs');
 
 
 app.use(helmet({crossOriginResourcePolicy: false}));
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'","*.fontawesome.com"],
-        scriptSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"],
-        styleSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"]
-    }
-}));
 
 app.use(cors({
     allowedHeaders: '*',
@@ -24,8 +17,6 @@ app.use(cors({
 }
 ));
 
-app.use(morgan('combined',{stream:fs.createWriteStream(join(__dirname,'./logs/access.log'), {flags:'a'})}));
-app.use(express.static(join(__dirname, './public')));
 app.use('/api/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
 app.use('/api/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
 app.get('*', (req,res) =>{
