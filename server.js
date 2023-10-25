6fe962097938930deebb01e9c336affa31673e06
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-10-25 13:05:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cors({
 }
 ));
 
-app.use('/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
-app.use('/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
+app.use('/api/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
+app.use('/api/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
 
 
 app.listen(3000,()=>console.log('API gateway listen on port 3000'));
