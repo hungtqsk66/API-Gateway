4ff68bbe6f63793ceffed6a4e43766ea0a657405
hungtqsk66 <nguyenthanhhungso@gmail.com>
2023-10-25 12:29:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
-const morgan = require('morgan');
 const {join}  = require('path');
-const fs = require('fs');
 
 
 app.use(helmet({crossOriginResourcePolicy: false}));
@@ -18,10 +16,6 @@ app.use(cors({
 ));
 
 app.use('/api/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
-app.use('/api/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
-app.get('*', (req,res) =>{
-    res.sendFile(join(__dirname,'./public/index.html'));
-});
 
 app.listen(3000,()=>console.log('API gateway listen on port 3000'));
 
