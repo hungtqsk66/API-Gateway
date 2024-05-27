e059414273096e775cdbe953b40ecbff1669aaf1
Hans.Nguyen <hans.nguyen@avepoint.com>
2024-05-27 16:50:19 +0700

    Revert "Update server.js"
    This reverts commit f62d21b5fb76b55eb28c009fb47c5549e45deb81.

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
 
+const {join}  = require('path');
+const fs = require('fs');
+
+
 app.use(helmet({crossOriginResourcePolicy: false}));
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'","*.fontawesome.com"],
+        scriptSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"],
+        styleSrc:["'self'", "'unsafe-inline'","*.fontawesome.com"]
+    }
+}));
 
 app.use(cors({
     allowedHeaders: '*',
@@ -14,9 +25,14 @@ app.use(cors({
 ));
 
 
-app.use('*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
+app.use(express.static(join(__dirname, './public')));
+app.use('/api/ml-server/*',createProxyMiddleware({target:'http://127.0.0.1:8000',changeOrigin:true}));
+app.use('/api/audio-server/*',createProxyMiddleware({target:'http://127.0.0.1:4000',changeOrigin:true}));
+app.get('*', (req,res) =>{
+    res.sendFile(join(__dirname,'./public/index.html'));
+});
 
+app.listen(3000,()=>console.log('API gateway listen on port 3000'));
 
-app.listen(3000,()=>console.log(`API gateway started on port 3000`));
 
 
